d124477ea3b593b1980afd376766c0b40335dd13
Katarzyna <katarzyna.kotynska@pagepro.pl>
2019-12-19 15:23:12 +0100

    corrected path and removed unused import

--- src/templates/post.js
+++ src/templates/post.js
@@ -1,6 +1,6 @@
 import React from 'react'
-import { graphql, Link } from 'gatsby'
-import Layout from '../components/Layout'
+import { graphql } from 'gatsby'
+import Layout from '../components/layout'
 
 const Template = ({ data }) => {
   const { markdownRemark: post } = data
