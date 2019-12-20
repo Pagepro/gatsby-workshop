b927a2a3ae292bd8044615f80dde348211c36426
Rafal Golubowicz <rafal.golubowicz@pagepro.pl>
2019-12-20 10:01:27 +0100

    step-0 fixed

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,14 +27,6 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `markdown-pages`,
-        path: `${__dirname}/src/markdown-pages`,
-      },
-    },
-    `gatsby-transformer-remark`,
     `gatsby-plugin-styled-components`
   ],
 }
