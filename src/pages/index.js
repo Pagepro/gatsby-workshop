3e3ccf1c096e79fe3c59ea6849e347a083bda140
Katarzyna <katarzyna.kotynska@pagepro.pl>
2019-12-21 13:12:03 +0100

    changed heading on index

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,7 +6,7 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
+    <h1>Dzień dobry</h1>
     <p>Welcome to your new Gatsby site.</p>
     <p>Now go build something great.</p>
   </Layout>
